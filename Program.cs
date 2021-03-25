9fd2b64e5528e83ef6427c895f4dea203a9bd866
103620582 <103620582@student.swin.edu.au>
2021-03-25 16:18:20 +1100

    Repetition Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RepetitionLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine("Please enter a number of times to repeat.");
+            string input2 = Console.ReadLine();
+            int number = int.Parse(input2);
+            
+            int counter = 0;
+
+            while (counter < number)
+            {
+                System.Console.WriteLine("Please enter a name.");
+                string name = Console.ReadLine();
+
+                counter++;
+            }
+
+            System.Console.WriteLine("Goodbye.");
+
+        
+        }
+    }
+}
+
